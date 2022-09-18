b28057d18a5ccc954d2f6447300c1f2a6d198d3b
juicyjake12 <108505695+juicyjake12@users.noreply.github.com>
2022-09-18 17:16:18 -0400

    finished readme

--- develop/index.js
+++ develop/index.js
@@ -19,9 +19,9 @@ function userInput(){
         },
         {
             type: "list",
-            name: "License",
+            name: "license",
             message: "what license do you want to use",
-            choices:["The MIT License", "The GPL License", "GNU License", "N/A"]
+            choices:["Academic Free License v3.0", "Apache license 2.0", "Artistic license 2.0", "Boost Software License 1.0", "BSD 2-clause license", "BSD 3-clause license", "Creative Commons Zero v1.0 Universal", "Creative Commons Attribution 4.0", "Creative Commons Attribution Share Alike 4.0", "Do What The Fuck You Want To Public License", "Educational Community License v2.0", "Eclipse Public License 1.0", "Eclipse Public License 2.0", "European Union Public License 1.1", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU General Public License v3.0",  "GNU Lesser General Public License v2.1", "GNU Lesser General Public License v3.0", "ISC", "LaTeX Project Public License v1.3c", "Microsoft Public License",  "MIT", "Mozilla Public License 2.0", "Open Software License 3.0", "SIL Open Font License 1.1", "University of Illinois/NCSA Open Source License", "The Unlicense", "zLib License"]
         },
         {
             type: "input",
@@ -30,18 +30,28 @@ function userInput(){
         },
         {
             type: "input",
-            name: "Email",
+            name: "email",
             message: "what is your email?"
         },
         {
             type: "input",
-            name: "Git",
+            name: "username",
             message: "what is your Github username?"
         },
         {
             type: "input",
             name: "installation",
             message: "How do you install your app?"
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "How to contribute?"
+        },
+        {
+            type: "input",
+            name: "testing",
+            message: "What tests did you have?"
         }
 
     ]
